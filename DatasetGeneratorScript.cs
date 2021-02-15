beb02df9d3d94c4838ef91da7f7f5c740dc22921
George-Valentin Hulpoi <armywwpt@gmail.com>
2021-02-16 00:56:11 +0200

    setare scena, lumini si camera

--- DatasetGeneratorScript.cs
+++ DatasetGeneratorScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -38,6 +38,10 @@ public class DatasetGeneratorScript : MonoBehaviour
             this.surpriseEmotion = new SurpriseEmotion(this.controllers);
             this.fearEmotion = new FearEmotion(this.controllers);
         }
+
+        this.ClearScene();
+        this.SetupLight();
+        this.SetupCamera();
     }
 
     void Update() 
@@ -103,5 +107,48 @@ public class DatasetGeneratorScript : MonoBehaviour
     {
         Transform transform = root.transform.childCount > 0 ? root.transform.GetChild(0) : null; 
         return (transform != null) ? GetLeafFromHierarchy(transform.gameObject) : root;
+    }
+
+    private void ClearScene()
+    {
+        List<string> targets = new List<string>
+            {"EventSystem", "Canvas", "Audio", "Ground", "Light Setups Stationary",
+            "Camera_FullShot_root", "Camera 4D_Closeup", "Timeline" };
+
+        foreach (string name in targets)
+        {
+            GameObject target = GameObject.Find(name);
+            if (target != null)
+            {
+                target.SetActive(false);
+            }
+        }
+    }
+
+    private void SetupLight()
+    {
+        GameObject light = GameObject.Find("Directional Light");
+
+        if (light != null)
+        {
+            light.transform.rotation = Quaternion.Euler(1, 0, 0);
+        }
+    }
+
+    private void SetupCamera()
+    {
+        GameObject focusTarget = GameObject.Find("EyeFocus");
+
+        if (focusTarget != null)
+        {
+            focusTarget.transform.position = new Vector3(0, 1.7f, 0);
+        }
+
+        GameObject camera = GameObject.Find("Main Camera");
+
+        if (camera != null)
+        {
+            camera.transform.position = new Vector3(0, 1.72f, -1.321f);
+        }
     }
 }
